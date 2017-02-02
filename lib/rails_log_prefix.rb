7658aee260434a5eeebca28574276a31c2f0322a
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-02-02 13:05:23 +0200

    Setup commit.

--- lib/rails_log_prefix.rb
+++ lib/rails_log_prefix.rb
@@ -1,4 +1,4 @@
-require "rails_log_prefix/version"
+require 'rails_log_prefix/version'
 
 module RailsLogPrefix
   # Your code goes here...
--- lib/rails_log_prefix/version.rb
+++ lib/rails_log_prefix/version.rb
@@ -1,3 +1,3 @@
 module RailsLogPrefix
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'
 end
