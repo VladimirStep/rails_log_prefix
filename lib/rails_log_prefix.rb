8b07fec06cb7c546f83cd03e91ec66d8aa7652fa
Vladimir Stepanov <vladimir.stepanov@faceit-team.com>
2017-02-07 13:09:27 +0200

    Add custom message after all Rails commands

--- lib/rails_log_prefix.rb
+++ lib/rails_log_prefix.rb
@@ -1,38 +1,15 @@
 require 'rails_log_prefix/railtie' if defined?(Rails)
 require 'rails_log_prefix/version'
-
-# require 'rails/railtie'
-# require 'rails/commands'
 require 'rails/commands/commands_tasks'
-# require 'rails/commands/server'
-
-
-# module RailsLogPrefix
-#   class Rails::Server
-#     alias_method :old_start, :start
-#
-#     def start
-#       puts 'MY CUSTOM MESSAGE'
-#       old_start
-#     end
-#   end
-# end
 
 module RailsLogPrefix
   Rails::CommandsTasks.class_eval do
+    alias_method :old_run, :run_command!
+    CUSTOM_MESSAGE = 'HELLO COMMAND'
+
     def run_command!(command)
-      puts "HELLO COMMAND"
-      command = parse_command(command)
-
-      if COMMAND_WHITELIST.include?(command)
-        send(command)
-      else
-        run_rake_task(command)
-      end
-    end
-
-    def server
-      puts "HELLO SERVER"
+      puts CUSTOM_MESSAGE
+      old_run(command)
     end
   end
 end
--- lib/rails_log_prefix/railtie.rb
+++ lib/rails_log_prefix/railtie.rb
@@ -1,5 +1,6 @@
+require 'rails/railtie'
+
 module RailsLogPrefix
   class Railtie < Rails::Railtie
-
   end
 end
